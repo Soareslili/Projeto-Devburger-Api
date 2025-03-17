e5882dac307a2c5a86f7083ec86f5a9d3ed1512e
Soareslili <lidianesantossoares2@gmail.com>
2025-03-17 16:53:59 -0300

    Criando pedidos no mongodb

---
+++ src/app/controllers/OrderController.js
@@ -0,0 +1,74 @@
+
+import * as Yup from 'yup'
+import Order from '../schema/Order'
+import Product from '../models/Product'
+import Category from '../models/Category'
+
+class OrderController {
+    async store(request, response) {
+        const schema = Yup.object({
+            products: Yup.array()
+                .required()
+                .of(
+                    Yup.object({
+                        id: Yup.number().required(),
+                        quantity: Yup.number().required(),
+                    })
+                ),
+        });
+
+        try {
+            schema.validateSync(request.body, { abortEarly: false });
+        } catch (err) {
+            return response.status(400).json({ error: err.errors });
+        }
+
+        const { products } = request.body;
+
+        const productsIds = products.map(product => product.id);
+
+        const findProducts = await Product.findAll({
+            where: {
+                id: productsIds,
+            },
+            include: [
+                {
+                    model: Category,
+                    as: 'category',
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const formattedProducts = findProducts.map(product => {
+           
+            const productQuantity = products.find(item => item.id === product.id)?.quantity;
+
+            const newProduct = {
+                id: product.id,
+                name: product.name,
+                category: product.category,
+                price: product.price,
+                url: product.url,
+                quantity: productQuantity, 
+            };
+
+            return newProduct;
+        });
+
+        const order = {
+            user: {
+                id: request.userId,
+                name: request.userName,
+                status: 'Pedido realizado'
+            },
+            products: formattedProducts,
+        };
+
+        const createOrder = await Order.create(order)
+
+        return response.status(201).json({ createOrder });
+    }
+}
+
+export default new OrderController;
--- src/database/index.js
+++ src/database/index.js
@@ -1,15 +1,18 @@
-import  Sequelize  from "sequelize";
-import  configDatabase  from '../config/database';
+import Sequelize from "sequelize";
+import configDatabase from '../config/database';
+
+import mongoose from 'mongoose';
 
 import User from "../app/models/User";
 import Product from "../app/models/Product";
 import Category from "../app/models/Category";
 
-const models = [User, Product, Category]
+const models = [User, Product, Category];
 
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
@@ -17,6 +20,12 @@ class Database {
         models
             .map((model) => model.init(this.connection))
             .map((model) => model.associate && model.associate(this.connection.models));
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/devburguer'
+        )
     }
 }
 
